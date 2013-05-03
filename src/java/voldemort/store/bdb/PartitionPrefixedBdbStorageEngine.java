1e73247f40f3bb9f800522c65216f7d8be540d76
Jay J Wylie <jay.j.wylie@gmail.com>
2013-05-03 10:43:14 -0700

    Fixed broken junit tests. Documented and commented out other parts of unit tests that rely on donor-based rebalancing or that rely on partition-stores being deleted during rebalance. Both features are currently (necessarily) broken.
    AdminClient
    - Fix latent bug that treated node ids as dense/contiguous
    
    RebalanceclusterPlan
    - minor refactor
    
    RebalanceController
    - handle the case of an "empty" plan differently. Because unneccessary moves are optimized out at plan time, some of the small tests now yield plans with no data movement. I.e., primary partition IDs move among nodes, but data does not because all nodes already host all data. This exposed a corner case in which rebalance was successful, but servers never updated their cluster xml with new partitoin ID mapping.
    - Removed random/unnecessary 10s sleep from control path. Annotated with a TODO for discussion to confirm this is right thing to do.
    
    DonorBasedRebalanceAsyncOperation
    - minor code clarification
    
    DonorBasedRebalancePusherSlave
    - Fixed latent bug (wrong value passed to sleep). Added TODO wondering why this sleep is here in the first place.
    
    StorageEngine
    - How are isPartitoinScanSupported() and isPartitionAware() related?
    
    PartitionPrefixedBdbStorageEngine
    - add isPartitionAware that returns true. Is this correct!?
    
    AdminRebalanceTest
    - Commented out asserts that test deletion during rebalancing
    
    ZonedRebalanceTest
    - Removed donor-based rebalancing tests from this test suite.

--- src/java/voldemort/store/bdb/PartitionPrefixedBdbStorageEngine.java
+++ src/java/voldemort/store/bdb/PartitionPrefixedBdbStorageEngine.java
@@ -277,6 +277,11 @@ public class PartitionPrefixedBdbStorageEngine extends BdbStorageEngine {
     }
 
     @Override
+    public boolean isPartitionAware() {
+        return true;
+    }
+
+    @Override
     public boolean isPartitionScanSupported() {
         return true;
     }
